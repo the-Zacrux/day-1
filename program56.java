a4acdaa6383ce95dfaf6f61ade318ce3f3fd11b3
the-Zacrux <divyanshshukla705@gmail.com>
2026-06-13 22:06:55 +0530

    Add files via upload

---
+++ program56.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class program56{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Duplicate elements are:");
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+        sc.close();
+    }
+}
